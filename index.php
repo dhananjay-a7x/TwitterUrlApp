cad2004bb7ff7a3b0f6e567c8703b1c1f9ff4b15
dhananjay-a7x <30922547+dhananjay-a7x@users.noreply.github.com>
2017-08-29 15:11:08 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>TweetURLApplicationTapzo</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+<a href="Documentation.html" class="myButton">Documentation</a>
+  <div class="wrapper">
+	<div class="container">
+		<h1>Enter your Twitter User ID</h1>
+		<form class="form" method="post" action="auth.php">
+			<input type="text" placeholder="Username">
+			<button type="submit" id="login-button">Login</button>
+		</form> 
+	</div>
+	
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+</html>
